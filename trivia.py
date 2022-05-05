70db4716c936e723ae6a2586ef60cfd75beb920e
blast <rodfval.fv@gmail.com>
2022-05-05 10:09:54 +0000

    implemented trivia api

--- trivia.py
+++ trivia.py
@@ -11,27 +11,25 @@ load_dotenv(dotenv_path)
 TMP_DIR = os.environ.get("TMP_DIR")
 TRIVIA_API = os.environ.get("TRIVIA_API")
 
-def get_quiz(author, amount, category, difficulty, typeq):
-  try:
-    if not author:
-      return "Error"
-    if amount and amount is not None and amount != "" and int(amount) <= 10:
-      url = TRIVIA_API + "?amount=" + amount
-    elif not amount or amount is None or amount == "":
-      url = TRIVIA_API + "?amount=5"
-    else:
-      return "Error"
-    if category and category is not None and category != "":
-      url = url + "&category=" + category
-    if difficulty and difficulty is not None and difficulty != "":
-      url = url + "&difficulty=" + difficulty
-    if typeq and typeq is not None and typeq != "":
-      url = url + "&typeq=" + typeq
-    response = requests.get(url)
-    return insert_new_quiz(author, response.json())
-  except:
-    print(traceback.format_exc())
-    return "Error"
+def create_quiz(author, author_id, amount, category, difficulty, typeq):
+  if not author:
+    raise Exception("Author is mandatory")
+  if not author_id:
+    raise Exception("Author id is mandatory")
+  if amount and amount is not None and amount != "" and int(amount) <= 10:
+    url = TRIVIA_API + "?amount=" + amount
+  elif not amount or amount is None or amount == "":
+    url = TRIVIA_API + "?amount=5"
+  else:
+    raise Exception("Amount must be an integer")
+  if category and category is not None and category != "":
+    url = url + "&category=" + category
+  if difficulty and difficulty is not None and difficulty != "":
+    url = url + "&difficulty=" + difficulty
+  if typeq and typeq is not None and typeq != "":
+    url = url + "&typeq=" + typeq
+  response = requests.get(url)
+  return create_new_quiz(author, author_id, response.json())
 
 def check_temp_trivia_exists(): 
   fle = Path(TMP_DIR+'trivia.sqlite3')
@@ -47,7 +45,8 @@ def create_empty_tables():
 
     sqlite_create_quiz_query = """ CREATE TABLE IF NOT EXISTS Quiz(
             id INTEGER PRIMARY KEY,
-            author VARCHAR(255) NOT NULL
+            author VARCHAR(255) NOT NULL,
+            author_id INTEGER NOT NULL
         ); """
 
     cursor.execute(sqlite_create_quiz_query)    
@@ -78,36 +77,48 @@ def create_empty_tables():
 
     cursor.execute(sqlite_create_answers_query)
 
+    sqlite_create_user_answers_query = """ CREATE TABLE IF NOT EXISTS Users(
+            id INTEGER PRIMARY KEY,
+            username VARCHAR(255) NOT NULL
+        ); """
+
+    cursor.execute(sqlite_create_user_answers_query)
+
     sqlite_create_user_answers_query = """ CREATE TABLE IF NOT EXISTS UserAnswers(
             id INTEGER PRIMARY KEY,
-            user VARCHAR(255) NOT NULL,
             user_id INTEGER NOT NULL,
             answer_id INTEGER NOT NULL,
+            question_id INTEGER NOT NULL,
+            FOREIGN KEY (user_id)
+              REFERENCES Users (id),
             FOREIGN KEY (answer_id)
-              REFERENCES Answers (id) 
+              REFERENCES Answers (id),
+            FOREIGN KEY (question_id)
+              REFERENCES Questions (question_id) 
         ); """
 
     cursor.execute(sqlite_create_user_answers_query)
 
 
   except sqlite3.Error as error:
-    print("Failed to create tables", error)
+    print("SQLITE Error: ", error)
+    raise Exception(str(error))
   finally:
     if sqliteConnection:
         sqliteConnection.close()
 
-def insert_new_quiz(author, content): 
+def create_new_quiz(author, author_id, content): 
   quiz_id = 0 
   try:
     sqliteConnection = sqlite3.connect(TMP_DIR+'trivia.sqlite3')
     cursor = sqliteConnection.cursor()
 
     sqlite_insert_quiz_query = """INSERT INTO Quiz
-                          (author) 
+                          (author, author_id) 
                            VALUES 
-                          (?)"""
+                          (?, ?)"""
 
-    data_quiz_tuple = (author,)
+    data_quiz_tuple = (author,author_id,)
 
     cursor.execute(sqlite_insert_quiz_query, data_quiz_tuple)
 
@@ -138,16 +149,20 @@ def insert_new_quiz(author, content):
     cursor.close()
 
   except sqlite3.Error as error:
-    print("Failed to insert data into sqlite", error)
+    print("SQLITE Error: ", error)
+    raise Exception(str(error))
   finally:
     if sqliteConnection:
-        sqliteConnection.close()
+        sqliteConnection.close()  
   if quiz_id != 0:
-    return get_quiz_json(quiz_id)
+    save_data_set = {
+        "Quiz_id":           quiz_id
+    }
+    return save_data_set
   else:
-    return "Error"
+    raise Exception("Error creating new quiz")
 
-def get_quiz_json(quiz_id):
+def get_quiz(quiz_id):
   quiz_data_set = None
   try:
 
@@ -164,8 +179,10 @@ def get_quiz_json(quiz_id):
     
     cursor_quiz.close()
 
-    if not author:
-      return "Error"
+    if not author:      
+      if sqliteConnection:
+        sqliteConnection.close()
+      raise Exception("Author not found")
 
     cursor_questions = sqliteConnection.cursor()
 
@@ -226,11 +243,211 @@ def get_quiz_json(quiz_id):
     }
 
   except sqlite3.Error as error:
-    print("Failed to read data from sqlite table", error)
+    print("SQLITE Error: ", error)
+    raise Exception(str(error))
   finally:
     if sqliteConnection:
       sqliteConnection.close()
 
   return quiz_data_set
+  if quiz_data_set is not None:
+    return quiz_data_set
+  else:
+    raise Exception("Quiz id not found")
+
+
+def save_answer(questionid: int, answerid: int, userid: int, username: str):
+  
+
+  answers_data_set = get_answer_internal(questionid, userid)
+
+  if(answers_data_set is not None):
+    save_data_set = {
+        "UserAnswers_id": answers_data_set['id']
+    }
+    return save_data_set
+
+
+  save_id = 0
+  try:
+    sqliteConnection = sqlite3.connect(TMP_DIR+'trivia.sqlite3')
+
+    save_user_no_commit(username, userid, sqliteConnection)
+
+    cursor = sqliteConnection.cursor()
+
+    sqlite_save_query = """INSERT INTO UserAnswers
+                          (user_id, answer_id, question_id) 
+                          VALUES 
+                          (?,?,?)"""
+
+    data = (userid,answerid,questionid,)
+    cursor.execute(sqlite_save_query, data)
+
+    save_id = cursor.lastrowid
+
+    sqliteConnection.commit()
+    cursor.close()
+
+  except sqlite3.Error as error:
+    print("SQLITE Error: ", error)
+    raise Exception(str(error))
+  finally:
+    if sqliteConnection:
+      sqliteConnection.close()
+  
+  if save_id != 0:
+    save_data_set = {
+        "UserAnswers_id":           save_id
+    }
+    return save_data_set
+  else:
+    raise Exception("Error saving the user answer")
+
+def save_user_commit(username: str, user_id: int):
+  save_id = 0
+  try:
+    sqliteConnection = sqlite3.connect(TMP_DIR+'trivia.sqlite3')
+    cursor_old = sqliteConnection.cursor()
+
+    old_user_id = None
+
+    sqlite_select_user_query = """SELECT * from Users WHERE id = ?"""
+    data_select = (user_id,)
+    cursor_old.execute(sqlite_select_user_query, data_select)
+    records_users = cursor_old.fetchall()
+
+    
+    for row in records_users:
+      save_id = row[0]
+
+
+    if save_id == 0:
+      cursor_old.close()
+
+      cursor = sqliteConnection.cursor()
+      sqlite_save_query = """INSERT INTO Users
+                            (id, username) 
+                            VALUES 
+                            (?, ?) """
+
+      data = (user_id,username,)
+      cursor.execute(sqlite_save_query, data)
+
+      save_id = cursor.lastrowid
+      cursor.close()
+      sqliteConnection.commit()
+
+  except sqlite3.Error as error:
+    print("SQLITE Error: ", error)
+    raise Exception(str(error))
+  finally:
+    if sqliteConnection:
+      sqliteConnection.close()
+  if save_id != 0:
+    save_data_set = {
+        "Users_id":           save_id
+    }
+    return save_data_set
+  else: 
+    raise Exception("Error saving the user")
+
+def save_user_no_commit(username: str, user_id: int, sqliteConnection):
+  save_id = 0
+  try:
+    cursor_old = sqliteConnection.cursor()
+
+    old_user_id = None
+
+    sqlite_select_user_query = """SELECT * from Users WHERE id = ?"""
+    data_select = (user_id,)
+    cursor_old.execute(sqlite_select_user_query, data_select)
+    records_users = cursor_old.fetchall()
+
+    
+    for row in records_users:
+      cursor_old.close()      
+      return
+
+    cursor_old.close()
+
+    cursor = sqliteConnection.cursor()
+    sqlite_save_query = """INSERT INTO Users
+                          (id, username) 
+                          VALUES 
+                          (?, ?) """
+
+    data = (user_id,username,)
+    cursor.execute(sqlite_save_query, data)
+
+    save_id = cursor.lastrowid
+    cursor.close()
+
+  except sqlite3.Error as error:
+    print("SQLITE Error: ", error)
+    raise Exception(str(error))
+  if save_id != 0:
+    return "User saved"
+  else: 
+    raise Exception("Error saving the user")
+
+
+def get_answer_internal(questionid, userid):
+  answers_data_set = None
+  try:
+    sqliteConnection = sqlite3.connect(TMP_DIR+'trivia.sqlite3')
+    cursor = sqliteConnection.cursor()
+
+    sqlite_select_answers_query = """SELECT * from UserAnswers WHERE question_id = ? and user_id = ? ORDER BY ID DESC"""
+
+    data = (questionid,userid,)
+    cursor.execute(sqlite_select_answers_query, data)
+
+    
+    records_user_answers = cursor.fetchall()
+
+    
+    for row in records_user_answers:
+      answer_id   =   row[3]
+    
+      cursor_answers = sqliteConnection.cursor()
+
+      sqlite_select_answers_query = """SELECT * from Answers WHERE id = ? ORDER BY ID DESC"""
+      cursor_answers.execute(sqlite_select_answers_query, (answer_id,))
+      records_answers = cursor_answers.fetchall()
+
+
+      for row in records_answers:
+        idanswer =     row[0]
+        answer =       row[1]
+        is_correct =   row[2]
+        
+        answers_data_set = {
+          "id":           idanswer, 
+          "answer":       answer, 
+          "is_correct":   is_correct
+        }
+
+        cursor_answers.close()
+
+    cursor.close()
+
+  except sqlite3.Error as error:
+    print("SQLITE Error: ", error)
+    raise Exception(str(error))
+  finally:
+    if sqliteConnection:
+      sqliteConnection.close()
+  
+  if answers_data_set:
+    return answers_data_set
+
+
+def get_answer(questionid, userid):
+  answers_data_set = get_answer_internal(questionid, userid)
+  if answers_data_set:
+    return answers_data_set
+  else:
+    raise Exception("Error getting the user answer")
 
   
--- trivia_api.py
+++ trivia_api.py
@@ -1,6 +1,6 @@
 import logging
 import trivia
-from flask import Flask, request, send_file, Response, jsonify
+from flask import Flask, request, send_file, Response, jsonify, abort, json
 from flask_restx import Api, Resource, reqparse
 logging.basicConfig(level=logging.ERROR)
 
@@ -12,24 +12,124 @@ trivia.create_empty_tables()
 app = Flask(__name__)
 api = Api(app)
 
-parsertriviaget = reqparse.RequestParser()
-parsertriviaget.add_argument("author")
-parsertriviaget.add_argument("amount")
-parsertriviaget.add_argument("category")
-parsertriviaget.add_argument("difficulty")
-parsertriviaget.add_argument("type")
+parserquizcreate = reqparse.RequestParser()
+parserquizcreate.add_argument("author", type=str)
+parserquizcreate.add_argument("author_id", type=int)
+parserquizcreate.add_argument("amount", type=int)
+parserquizcreate.add_argument("category", type=str)
+parserquizcreate.add_argument("difficulty", type=str)
+parserquizcreate.add_argument("type", type=str)
 
 nstrivia = api.namespace('trivia', 'Accumulators Chatbot Trivia APIs')
-@nstrivia.route('/getquiz')
-class TriviaGet(Resource):
-  @api.expect(parsertriviaget)
+@nstrivia.route('/quiz/create')
+class QuizCreate(Resource):
+  @api.expect(parserquizcreate)
+  @api.response(200, 'Success')
+  @api.response(400, 'Generic Error')
   def get(self):
-    author = request.args.get("author")
-    amount = request.args.get("amount")
-    category = request.args.get("category")
-    difficulty = request.args.get("difficulty")
-    typeq = request.args.get("type")
-    return trivia.get_quiz(author, amount, category, difficulty, typeq)
+    try:
+      author = request.args.get("author")
+      author_id = request.args.get("author_id")
+      amount = request.args.get("amount")
+      category = request.args.get("category")
+      difficulty = request.args.get("difficulty")
+      typeq = request.args.get("type")
+      quiz_data = trivia.create_quiz(author, author_id, amount, category, difficulty, typeq)
+      resp = Response(json.dumps(quiz_data), mimetype='application/json')
+      resp.status_code = 200
+      return resp
+    except Exception as e:
+      abort(400, str(e))
+      
+parserquizinsert = reqparse.RequestParser()
+parserquizinsert.add_argument("quiz_id", type=int)
+
+@nstrivia.route('/quiz/get')
+class QuizInsert(Resource):
+  @api.expect(parserquizinsert)
+  @api.response(200, 'Success')
+  @api.response(400, 'Generic Error')
+  def get(self):
+    try:
+      quiz_id = request.args.get("quiz_id")
+      quiz_data = trivia.get_quiz(quiz_id)
+      resp = Response(json.dumps(quiz_data), mimetype='application/json')
+      resp.status_code = 200
+      return resp
+    except Exception as e:
+      abort(400, str(e))
+
+
+parserusersaveanswer = reqparse.RequestParser()
+parserusersaveanswer.add_argument("questionid", type=int)
+parserusersaveanswer.add_argument("answerid", type=int)
+parserusersaveanswer.add_argument("userid", type=int)
+parserusersaveanswer.add_argument("username", type=str)
+
+@nstrivia.route('/user/saveanswer')
+class UserSaveAnswer(Resource):
+  @api.expect(parserusersaveanswer)
+  @api.response(200, 'Success')
+  @api.response(400, 'Generic Error')
+  def get(self):
+    try:
+      questionid = request.args.get("questionid")
+      answerid = request.args.get("answerid")
+      userid = request.args.get("userid")
+      username = request.args.get("username")
+      save_data = trivia.save_answer(questionid, answerid, userid, username)
+      resp = Response(json.dumps(save_data), mimetype='application/json')
+      resp.status_code = 200
+      return resp
+    except Exception as e:
+      abort(400, str(e))
+
+
+
+
+parserusersave = reqparse.RequestParser()
+parserusersave.add_argument("userid", type=int)
+parserusersave.add_argument("username", type=str)
+
+@nstrivia.route('/user/saveuser')
+class UserSave(Resource):
+  @api.expect(parserusersave)
+  @api.response(200, 'Success')
+  @api.response(400, 'Generic Error')
+  def get(self):
+    try:
+      userid = request.args.get("userid")
+      username = request.args.get("username")
+      save_data = trivia.save_user_commit(username, userid)
+      resp = Response(json.dumps(save_data), mimetype='application/json')
+      resp.status_code = 200
+      return resp
+    except Exception as e:
+      abort(400, str(e))
+
+
+
+
+parserusergetanswer = reqparse.RequestParser()
+parserusergetanswer.add_argument("questionid", type=int)
+parserusergetanswer.add_argument("userid", type=int)
+
+@nstrivia.route('/user/getanswer')
+class UserGetAnswer(Resource):
+  @api.expect(parserusergetanswer)
+  @api.response(200, 'Success')
+  @api.response(400, 'Generic Error')
+  def get(self):
+    try:
+      questionid = request.args.get("questionid")
+      userid = request.args.get("userid")
+      get_data = trivia.get_answer(questionid, userid)
+      resp = Response(json.dumps(get_data), mimetype='application/json')
+      resp.status_code = 200
+      return resp
+    except Exception as e:
+      abort(400, str(e))
+
 
 if __name__ == '__main__':
   app.run()
